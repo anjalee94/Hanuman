1f6c64a9575f4707d313d970b8e1f2efe5abb2c1
Your Name <you@example.com>
2020-04-10 18:04:21 +0530

    modify SortingPGM.java

--- SortingPGM.java
+++ SortingPGM.java
@@ -1,17 +1,42 @@
 package co.edu;
 
 import java.util.ArrayList;
+import java.util.Collections;
 
 public class SortingPGM {
 
 	public static void main(String[] args) {
 		ArrayList<AuthorComparator> arr1 = new ArrayList<AuthorComparator>();
-		arr1.add(new AuthorComparator("Henry","Marto",54));
-		arr1.add(new AuthorComparator("Henna","Maria",34));
-		arr1.add(new AuthorComparator("Pengi","Manhotra",39));
-		arr1.add(new AuthorComparator("Hyper","Marino",65));
-		arr1.add(new AuthorComparator("Prem","Fogat",74));
-		arr1.add(new AuthorComparator("anji","gupta",35));
+		arr1.add(new AuthorComparator("Henry","Math",54));
+		arr1.add(new AuthorComparator("Henna","History",34));
+		arr1.add(new AuthorComparator("Pengi","Science",39));
+		arr1.add(new AuthorComparator("Hyper","English",65));
+		arr1.add(new AuthorComparator("Prem","Social",74));
+		arr1.add(new AuthorComparator("anji","Hindi",35));
+		
+		System.out.println("Sorting by Author First Name:");
+		System.out.println();
+		Collections.sort(arr1);
+		for(AuthorComparator asort:arr1) { 
+		System.out.println(asort.getFirstName() +", "+asort.getBookName()+", "+asort.getAuthAge());
+		}
+		System.out.println();
+		
+		System.out.println("Sorting by Author Age:");
+		System.out.println();
+		Collections.sort(arr1, new AuthorAgeComparator());
+		for(AuthorComparator asort:arr1){
+			System.out.println(asort.getFirstName() +", "+asort.getBookName()+", "+asort.getAuthAge());
+			
+		}
+		System.out.println();
+		System.out.println("Sorting by Author Book Name:");
+		System.out.println();
+		Collections.sort(arr1, new BookNameComparator());
+		for(AuthorComparator asort:arr1) { 
+		System.out.println(asort.getFirstName() +", "+asort.getBookName()+", "+asort.getAuthAge());
+		}
+		
 
 	}
 
